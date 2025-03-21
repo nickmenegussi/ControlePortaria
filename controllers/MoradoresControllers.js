1867f22451d8de5213559c7628fd01985375dd0b
NICOLAS MENEGUSSI RAMOS <05778187084@senacrs.edu.br>
2025-03-21 11:58:14 -0300

    verificando erros na criação de moradores e adiconando novas lógicas

--- controllers/MoradoresControllers.js
+++ controllers/MoradoresControllers.js
@@ -1,7 +1,19 @@
 const connection = require('../config/db')
 
 exports.viewMorador = (req, res) => {
-    const {} = req.body
+    connection.query('SELECT * FROM Moradores', (err, result) => {
+        if(err){
+            return res.status(500).json({
+                message: 'Erro ao se conectar com o servidor',
+                success: false,
+                data: err
+            })
+        }
+
+        return res.status(200).json({
+            message: 'Sucesso ao exibir os Moradores.'
+        })
+    })
 }
 
 exports.CreateMorador = (req, res) => {
@@ -36,7 +48,8 @@ exports.CreateMorador = (req, res) => {
                 success: false
             })
         }
-        connection.query('INSERT INTO Moradores(nome, bloco, apartamento, telefone, email, status) values(?, ?, ?, ?, ?, ?)', [nome, bloco, apartamento, telefone, email, status], (err, result) => {
+
+        connection.query(`SELECT * FROM Moradores where status = 'residente'`, [status], (err,result) => {
             if(err){
                 return res.status(500).json({
                     message: 'Erro ao se conectar com o servidor',
@@ -44,12 +57,65 @@ exports.CreateMorador = (req, res) => {
                     data: err
                 })
             }
+            if(result.length > 0){
+                return res.status(422).json({
+                    message: 'Este condominio já possui 1 propietario.'
+                })
+            }
 
-            return res.status(200).json({
-                message: 'Morador criador com sucesso.',
-                success: true,
-                data: result
+            connection.query('INSERT INTO Moradores(nome, bloco, apartamento, telefone, email, status) values(?, ?, ?, ?, ?, ?)', [nome, bloco, apartamento, telefone, email, status], (err, result) => {
+                if(err){
+                    return res.status(500).json({
+                        message: 'Erro ao se conectar com o servidor',
+                        success: false,
+                        data: err
+                    })
+                }
+    
+                return res.status(200).json({
+                    message: 'Morador criador com sucesso.',
+                    success: true,
+                    data: result
+                })
             })
         })
+        
+    })
+}
+
+exports.UpdateMoradores = (req, res) => {
+    const {idVeiculos} = req.params
+    const {nome, bloco, apartamento, telefone, email, status} = req.body
+
+    if(!nome || !bloco || !apartamento || !telefone || !email || !status || !idVeiculos){
+        return res.status(400).json({
+            messasge: 'Preencha todos os campos.',
+            success: false
+        })
+    }
+
+    if(status !== 'residente' && status !== 'proprietario' && status !== 'visitante'){
+        return res.status(400).json({
+            message: 'Selecione um valor válido do nosso menu.',
+            success: false
+        })
+    }
+
+    connection.query('SELECT * FROM Moradores where idMoradores = ?', [idVeiculos], (err, result) => {
+        if(err){
+            return res.status(500).json({
+                message: 'Erro ao se conectar com o servidor',
+                success: false,
+                data: err
+            })
+        }
+
+        if(result.length === 0){
+            return res.status(400).json({
+                message: 'Morador não existe.',
+                success: false
+            })
+        }
+        connection.query('UPDATE Moradores set nome = ?, bloco = ?, apartamento = ?, telefone = ?, email = ?, status = ?',[nome, bloco, apartamento, telefone,  email, status ])
     })
 }
