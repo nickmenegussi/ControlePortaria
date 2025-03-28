962eb8159a65b689caaef967a452ec179390abcd
NICOLAS MENEGUSSI RAMOS <05778187084@senacrs.edu.br>
2025-03-28 10:48:42 -0300

    Adicionando Novas lógicas ao backend

--- BackEnd/routers/VeiculoRouter.js
+++ BackEnd/routers/VeiculoRouter.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {ViewVeiculos, CreateVeiculos} = require('../controllers/VeiculosControllers')
+const {ViewVeiculos, CreateVeiculos, UpdateVeiculos, DeleteVeiculos} = require('../controllers/VeiculosControllers')
 
 router.get('/veiculos', ViewVeiculos)
-router.put('/veiculos/:idVeiculos',)
+router.put('/veiculos/:idVeiculos', UpdateVeiculos)
 
 router.post('/veiculos/create', CreateVeiculos)
-router.delete('/veiculos/:idVeiculos',)
+router.delete('/veiculos/:idVeiculos', DeleteVeiculos)
 
 module.exports = router
